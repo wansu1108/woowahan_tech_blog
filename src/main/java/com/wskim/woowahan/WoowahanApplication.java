461e6fab9303865059521a90b45440253e773757
wskim <wansuk1108@gmail.com>
2024-05-13 21:57:55 +0900

    Project First commit

---
+++ src/main/java/com/wskim/woowahan/WoowahanApplication.java
@@ -0,0 +1,13 @@
+package com.wskim.woowahan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowahanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wskim/woowahan/WoowahanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wskim.woowahan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowahanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
